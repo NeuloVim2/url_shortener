ebcb565d6b71f6dc0604ae71955f9cf650143116
stanislav <strelcov.stanislav93@gmail.com>
2021-07-12 16:43:18 +0300

    project initialization

---
+++ server.js
@@ -0,0 +1,29 @@
+const urlShortener = require('./url_shortener'),
+      db           = require('./db/mongoose');
+
+require('dotenv').config();
+const express = require('express'),
+      cors = require('cors'),
+      app = express();
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use('/api/shorturl/', urlShortener);
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.listen(port, function() {
+  console.log(`Listening on port http://localhost:${port}`);
+});
