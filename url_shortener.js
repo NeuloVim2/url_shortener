b0afc3c6a9b5f236251964ef02dd8cf70df10b9a
stanislav <strelcov.stanislav93@gmail.com>
2021-07-15 19:48:09 +0300

    fixed regex for url format

--- url_shortener.js
+++ url_shortener.js
@@ -5,7 +5,7 @@ const express = require("express"),
   shortenerUrlSchema = require("./db/schemas/shorter_url.schema");
 
 // const urlFormat = /(http[s]?:\/\/)?((www\.)[a-zA-Z]+(\.[a-z]{2,3}\/?)$)|((http[s]?:\/\/)?([a-zA-Z]+)(\.[a-z]{2,3}\/?)$)|((http[s]?:\/\/)?(www\.)?([a-zA-Z]+)(\.[a-z]{2,3}\/?)(\/[a-z]+\/?)+$)/
-const urlFormat = /http[s]?:\/\/www\.[a-zA-Z]+\.[a-z]{3}$/;
+const urlFormat = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
 
 const UrlShortener = mongoose.model("UrlShortener", shortenerUrlSchema);
 
@@ -20,7 +20,7 @@ urlShortener.get("/", (req, res) => {
 urlShortener.post("/", (req, res) => {
   let url = req.body.url;
   let result = urlFormat.test(req.body.url);
-  console.log(url);
+  console.log(`url parsed from request - ${url}`);
   if (result) {
     console.log("url format test is passed");
     UrlShortener.exists({ original_url: url }, (err, result) => {
@@ -39,7 +39,7 @@ urlShortener.post("/", (req, res) => {
           });
       } else {
         console.log(`creating new documnet`)
-        UrlShortener.create({ original_url: url, short_url: add() }, function (err, doc) {
+        UrlShortener.create({ original_url: url, short_url: getRandomIntInclusive(1, 100) }, function (err, doc) {
           if (err) {
            return console.log(`failed to create document, returned such error ${err}`);
           }
@@ -59,19 +59,21 @@ urlShortener.post("/", (req, res) => {
 
 urlShortener.get("/:short_url", (req, res) => {
   let shortUrlIndex = req.params.short_url;
-  console.log(shortUrlIndex);
-  UrlShortener.findOne({ short_url: shortUrlIndex }).then((err, doc) => {
-    if (!err) {
+  console.log(`value of :shor_url ${shortUrlIndex}`);
+  UrlShortener.findOne({ short_url: shortUrlIndex }).then((doc) => {
       console.log(doc.original_url);
       res.redirect(doc.original_url);
-    }
-  });
+    })
+    .catch((err) => {
+      console.log(`unable to find corresponding document to this ${shortUrlIndex} short_url`)
+    });
 });
 
 
-const add = (function () {
-  let counter = 0;
-  return function () {counter += 1; return counter}
-})()
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 module.exports = urlShortener;
